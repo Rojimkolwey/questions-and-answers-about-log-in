52827ef8fe1c43cf175d56e023bf584fcc5a959e
Rojimkolwey <rodgersmkolwe@gmail.com>
2023-02-04 15:07:08 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const plusbtns = document.querySelectorAll(".plus-btn");
+const contents = document.querySelectorAll(".content");
+
+console.log(plusbtns);
+
+const openCard = () => {
+  for (let index = 0; index < plusbtns.length; index++) {
+    plusbtns[index].addEventListener("click", () => {
+      contents.forEach((content, contentIndex) => {
+        if (!(index === contentIndex)) {
+          content.classList.remove("show");
+        }
+      });
+
+      contents[index].classList.toggle("show");
+    });
+  }
+};
+
+openCard();
+
+// plusbtns.forEach((btn, index) => {
+//   btn.addEventListener("click", () => {
+//     contents.forEach((content, contentIndex) => {
+//       if (index === contentIndex) {
+//         content.classList.toggle("show");
+//       }
+//     });
+//   });
+// });
